a67582a61787b22192274911dff9554acc755128
Ted Kruse <tpk5348@psu.edu>
2022-10-11 15:55:03 -0400

    Added follows and bourbons owned to profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -17,6 +17,9 @@ export class ProfileComponent implements OnInit {
   public profileName: string;
   public reviews: Review[];
   public bourbons: Bourbon[];
+  public hasBourbons: Boolean;
+  public hasFollowed: Boolean;
+  public hasReviewed: Boolean;
 
   constructor(private profileService: ProfileService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
@@ -32,6 +35,18 @@ export class ProfileComponent implements OnInit {
     this.profileService.getProfileByName(this.profileName).subscribe(
       (response: Profile)=> {
         this.profile = response;
+        if(this.profile.bourbon_ids != undefined && this.profile.bourbon_ids.length != 0){
+          this.hasBourbons = true;
+        }
+        else{
+          this.hasBourbons = false;
+        }
+        if(this.profile.followed_names != undefined && this.profile.followed_names.length != 0){
+          this.hasFollowed = true;
+        }
+        else{
+          this.hasFollowed = false;
+        }
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
@@ -43,6 +58,12 @@ export class ProfileComponent implements OnInit {
     this.profileService.getReviewsOnProfile(this.profileName).subscribe(
       (response: Review[])=> {
         this.reviews = response;
+        if(this.reviews != undefined && this.reviews.length != 0){
+          this.hasReviewed = true;
+        }
+        else{
+          this.hasReviewed = false;
+        }
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
