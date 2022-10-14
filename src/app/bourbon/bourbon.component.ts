27125ee81852ca94b140020fb6bc285843958641
Ted Kruse <tpk5348@psu.edu>
2022-10-13 20:06:18 -0400

    Added sorting to the bourbons page

--- src/app/bourbon/bourbon.component.ts
+++ src/app/bourbon/bourbon.component.ts
@@ -28,4 +28,91 @@ export class BourbonComponent implements OnInit {
       );
 
   }
+
+  sortBourbonsRating(){
+    var len = this.bourbons.length;
+    var i;
+    var j;
+    var stop;
+    for (i=0; i < len; i++){
+      for (j=0, stop=len-i; j < stop; j++){
+        if (this.bourbons[j]?.rating != undefined && typeof this.bourbons[j+1]?.rating != undefined){
+          if (this.bourbons[j]?.rating < this.bourbons[j+1]?.rating){
+            this.swap(this.bourbons, j, j+1);
+          };
+        }
+        else if (this.bourbons[j+1]?.rating != undefined){
+          this.swap(this.bourbons, j, j+1);
+        }
+        
+      };
+    };
+  }
+
+  sortBourbonsProof(){
+    var len = this.bourbons.length;
+    var i;
+    var j;
+    var stop;
+    for (i=0; i < len; i++){
+      for (j=0, stop=len-i; j < stop; j++){
+        if (this.bourbons[j]?.proof != undefined && typeof this.bourbons[j+1]?.proof != undefined){
+          if (this.bourbons[j]?.proof < this.bourbons[j+1]?.proof){
+            this.swap(this.bourbons, j, j+1);
+          };
+        }
+        else if (this.bourbons[j+1]?.proof != undefined){
+          this.swap(this.bourbons, j, j+1);
+        }
+        
+      };
+    };
+  }
+
+  sortBourbonsName(){
+    var len = this.bourbons.length;
+    var i;
+    var j;
+    var stop;
+    for (i=0; i < len; i++){
+      for (j=0, stop=len-i; j < stop; j++){
+        if (this.bourbons[j]?.name != undefined && typeof this.bourbons[j+1]?.name != undefined){
+          if (this.bourbons[j]?.name > this.bourbons[j+1]?.name){
+            this.swap(this.bourbons, j, j+1);
+          };
+        }
+        else if (this.bourbons[j+1]?.name != undefined){
+          this.swap(this.bourbons, j, j+1);
+        }
+        
+      };
+    };
+  }
+
+  sortBourbonsDistil(){
+    var len = this.bourbons.length;
+    var i;
+    var j;
+    var stop;
+    for (i=0; i < len; i++){
+      for (j=0, stop=len-i; j < stop; j++){
+        if (this.bourbons[j]?.distil != undefined && typeof this.bourbons[j+1]?.distil != undefined){
+          if (this.bourbons[j]?.distil > this.bourbons[j+1]?.distil){
+            this.swap(this.bourbons, j, j+1);
+          };
+        }
+        else if (this.bourbons[j+1]?.distil != undefined){
+          this.swap(this.bourbons, j, j+1);
+        }
+        
+      };
+    };
+  }
+
+  swap(sortedBourbons: Bourbon[], firstIndex: number, secondIndex: number){
+    var temp = sortedBourbons[firstIndex];
+    sortedBourbons[firstIndex] = sortedBourbons[secondIndex];
+    sortedBourbons[secondIndex] = temp;
+    }
+
 }
