2273606516fdcbf654f2c68823fed6470b76032e
Ted Kruse <tpk5348@psu.edu>
2022-11-10 19:12:32 -0500

    Added top ten bourbons to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Bourbon } from '../bourbon';
+import { BourbonService } from '../bourbon.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public bourbons: Bourbon[];
+  public bourbonsLen: number;
+
+  constructor(private bourbonService: BourbonService) { }
 
   ngOnInit(): void {
+    this.getBourbons();
+
   }
+
+  public getBourbons(): void {
+    this.bourbonService.getBourbons().subscribe(
+      (response: Bourbon[]) => {
+        this.bourbons = response;
+        this.bourbonsLen = this.bourbons.length;
+        this.sortBourbonsRating();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+      );
+  }
+
+  sortBourbonsRating(){
+    var len = this.bourbons.length;
+    var i;
+    var j;
+    var stop;
+    for (i=0; i < len; i++){
+      for (j=0, stop=len-i; j < stop; j++){
+        if (this.bourbons[j]?.rating != undefined && typeof this.bourbons[j+1]?.rating != undefined){
+          if (this.bourbons[j]?.rating < this.bourbons[j+1]?.rating){
+            this.swap(this.bourbons, j, j+1);
+          };
+        }
+        else if (this.bourbons[j+1]?.rating != undefined){
+          this.swap(this.bourbons, j, j+1);
+        }
+        
+      };
+    };
+  }
+
+  swap(sortedBourbons: Bourbon[], firstIndex: number, secondIndex: number){
+    var temp = sortedBourbons[firstIndex];
+    sortedBourbons[firstIndex] = sortedBourbons[secondIndex];
+    sortedBourbons[secondIndex] = temp;
+    }
 
 }
