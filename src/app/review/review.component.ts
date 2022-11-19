7471129957494a8709aee636aa0314946a255483
Ted Kruse <tpk5348@psu.edu>
2022-11-18 20:18:43 -0500

    Added sorting for reviews

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -30,4 +30,132 @@ export class ReviewComponent implements OnInit {
       );
     }
 
+    sortReviewsRating(){
+      var len = this.reviews.length;
+      var i;
+      var j;
+      var stop;
+      for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+          if (this.reviews[j]?.rating != undefined && typeof this.reviews[j+1]?.rating != undefined){
+            if (this.reviews[j]?.rating < this.reviews[j+1]?.rating){
+              this.swap(this.reviews, j, j+1);
+            };
+          }
+          else if (this.reviews[j+1]?.rating != undefined){
+            this.swap(this.reviews, j, j+1);
+          }
+          
+        };
+      };
+    }
+
+    sortReviewsTaste(){
+      var len = this.reviews.length;
+      var i;
+      var j;
+      var stop;
+      for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+          if (this.reviews[j]?.taste != undefined && typeof this.reviews[j+1]?.taste != undefined){
+            if (this.reviews[j]?.taste < this.reviews[j+1]?.taste){
+              this.swap(this.reviews, j, j+1);
+            };
+          }
+          else if (this.reviews[j+1]?.taste != undefined){
+            this.swap(this.reviews, j, j+1);
+          }
+          
+        };
+      };
+    }
+
+    sortReviewsNose(){
+      var len = this.reviews.length;
+      var i;
+      var j;
+      var stop;
+      for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+          if (this.reviews[j]?.nose != undefined && typeof this.reviews[j+1]?.nose != undefined){
+            if (this.reviews[j]?.nose < this.reviews[j+1]?.nose){
+              this.swap(this.reviews, j, j+1);
+            };
+          }
+          else if (this.reviews[j+1]?.nose != undefined){
+            this.swap(this.reviews, j, j+1);
+          }
+          
+        };
+      };
+    }
+
+    sortReviewsMouthfeel(){
+      var len = this.reviews.length;
+      var i;
+      var j;
+      var stop;
+      for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+          if (this.reviews[j]?.mouthfeel != undefined && typeof this.reviews[j+1]?.mouthfeel != undefined){
+            if (this.reviews[j]?.mouthfeel < this.reviews[j+1]?.mouthfeel){
+              this.swap(this.reviews, j, j+1);
+            };
+          }
+          else if (this.reviews[j+1]?.mouthfeel != undefined){
+            this.swap(this.reviews, j, j+1);
+          }
+          
+        };
+      };
+    }
+
+    sortReviewsValue(){
+      var len = this.reviews.length;
+      var i;
+      var j;
+      var stop;
+      for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+          if (this.reviews[j]?.value != undefined && typeof this.reviews[j+1]?.value != undefined){
+            if (this.reviews[j]?.value < this.reviews[j+1]?.value){
+              this.swap(this.reviews, j, j+1);
+            };
+          }
+          else if (this.reviews[j+1]?.value != undefined){
+            this.swap(this.reviews, j, j+1);
+          }
+          
+        };
+      };
+    }
+
+    sortReviewsAvail(){
+      var len = this.reviews.length;
+      var i;
+      var j;
+      var stop;
+      for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+          if (this.reviews[j]?.availability != undefined && typeof this.reviews[j+1]?.availability != undefined){
+            if (this.reviews[j]?.availability < this.reviews[j+1]?.availability){
+              this.swap(this.reviews, j, j+1);
+            };
+          }
+          else if (this.reviews[j+1]?.availability != undefined){
+            this.swap(this.reviews, j, j+1);
+          }
+          
+        };
+      };
+    }
+
+    swap(sortedReviews: Review[], firstIndex: number, secondIndex: number){
+      var temp = sortedReviews[firstIndex];
+      sortedReviews[firstIndex] = sortedReviews[secondIndex];
+      sortedReviews[secondIndex] = temp;
+      }
+  
+
+
 }
